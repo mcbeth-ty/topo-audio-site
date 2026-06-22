1d85d92607484bf6e4d6724ac6e10a2e99aa3327
Tyler McBeth <mcbeth.ty@gmail.com>
2026-06-22 22:53:41 +0900

    Create project architecture and clean starter template

---
+++ src/data/sections.js
@@ -0,0 +1,16 @@
+export const sections = [
+  "intro",
+  "problem",
+  "hypothesis",
+  "why-harmony",
+  "methodology",
+  "scales",
+  "collecting-perceptions",
+  "creating-stimuli",
+  "constructing-topologies",
+  "analysis",
+  "interactive-prototypes",
+  "research-progress",
+  "ethics",
+  "about"
+];
